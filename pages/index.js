86c550d60693b28ba97a135f446912d615e4a337
Darsshan <darsshanpalaram@gmail.com>
2023-02-02 17:15:02 +0000

    Changes navbar

--- pages/index.js
+++ pages/index.js
@@ -26,23 +26,12 @@ export default function Home() {
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section>
           <nav className="py-10 mb-0 flex justify-between">
-            <h1 className="text-xl font-burtons dark:text-white">
-              PersonalWebpage
-            </h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-                  className="cursor-pointer text-2xl"
+                  className="cursor-pointer text-2xl dark:text-white"
                 />
-              </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
               </li>
             </ul>
           </nav>
